573885b1dd0f09ca5ace118ece1955fb704de763
Ervey Garcia <ervey.garcia@carrier.com>
2025-04-20 15:07:52 -0600

    fix: remove .tsx extensions to fix Render build

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,13 +2,13 @@ import * as ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 
-import App from './App.tsx';
-import Board from './pages/Board.tsx';
-import ErrorPage from './pages/ErrorPage.tsx';
-import EditTicket from './pages/EditTicket.tsx';
-import CreateTicket from './pages/CreateTicket.tsx';
-import Login from './pages/Login.tsx';
-import ProtectedRoute from './components/ProtectedRoute.tsx';
+import App from './App';
+import Board from './pages/Board';
+import ErrorPage from './pages/ErrorPage';
+import EditTicket from './pages/EditTicket';
+import CreateTicket from './pages/CreateTicket';
+import Login from './pages/Login';
+import ProtectedRoute from './components/ProtectedRoute';
 
 const router = createBrowserRouter([
   {
@@ -19,31 +19,30 @@ const router = createBrowserRouter([
       {
         index: true,
         element: (
-        <ProtectedRoute>
-         <Board />
-         </ProtectedRoute>
+          <ProtectedRoute>
+            <Board />
+          </ProtectedRoute>
         ) 
       }, 
       {
         path: '/edit',
         element: (
-            <ProtectedRoute>
-              <EditTicket />
-            </ProtectedRoute>
+          <ProtectedRoute>
+            <EditTicket />
+          </ProtectedRoute>
         )
       },
       {
         path: '/create',
         element: (
-        <ProtectedRoute>
-          <CreateTicket />
+          <ProtectedRoute>
+            <CreateTicket />
           </ProtectedRoute>
         )  
       },
       {
         path: '/login',
         element: <Login />
-        
       }
     ]
   }
