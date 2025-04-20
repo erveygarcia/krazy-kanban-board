26ca2245f3e13fd9d573cdfd8cd7e289a0d18bb3
Ervey Garcia <ervey.garcia@carrier.com>
2025-04-20 13:12:20 -0600

    Configure Sequelize for Render Postgres

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -5,8 +5,16 @@ import { Sequelize } from 'sequelize';
 import { UserFactory } from './user';
 import { TicketFactory } from './ticket';
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    })
   : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'postgres',
@@ -14,6 +22,7 @@ const sequelize = process.env.DB_URL
         decimalNumbers: true,
       },
     });
+
 
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
